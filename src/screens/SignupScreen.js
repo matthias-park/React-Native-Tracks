d32d1bafde191b71b7fe7c0a1fb2797d2b3eaf29
matthias <matthias.dfrc.ch>
2020-01-06 17:36:58 +0900

    2020.01.06_01 1. Set boilerplate of initializing 2. added navigations

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,54 @@
+import React, {useState } from 'react';
+import { View, StyleSheet} from 'react-native';
+import { Text, Input, Button} from 'react-native-elements';
+import Spacer from '../Components/Spacer';
+
+const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <Spacer>
+                <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input 
+                label='Email' 
+                value={email} 
+                onChangeText={setEmail}
+                autoCapitalize='none'
+                autoCorrect={false}
+            />
+            <Spacer />
+            <Input 
+                secureTextEntry
+                label='Password' 
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize='none'
+                autoCorrect={false}
+            />
+            <Spacer>
+                <Button title='Sign Up' />
+            </Spacer>
+        </View>
+    )
+};
+
+SignupScreen.navigationOptions = () => {
+    return {
+        header: null
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderColor: 'red',
+        borderWidth: 10,
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    }
+});
+
+export default SignupScreen;
